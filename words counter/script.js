fd371063a3d34292f50f782177514e6efd4b1823
Aswanthrajan <aswanthrajan0@gmail.com>
2025-09-25 21:43:39 +0530

    new project added

---
+++ words counter/script.js
@@ -0,0 +1,12 @@
+
+const textarea = document.querySelector('textarea');
+const counter = document.querySelector('.counter');
+
+function countingCharacter(){
+
+
+    const textLength = textarea.value.length;
+    console.log(textLength);
+
+    counter.innerText = `${textLength}`;
+}
